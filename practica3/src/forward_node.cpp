b1be693704b9416d3320ff9c330e41a7c8f9745a
Blancasr <b.soriar.2019@alumnos.urjc.es>
2021-03-23 21:19:45 +0100

    cawen make
    Signed-off-by: Blancasr <b.soriar.2019@alumnos.urjc.es>

--- practica3/src/forward_node.cpp
+++ practica3/src/forward_node.cpp
@@ -1,4 +1,4 @@
-#include "practica3/Forward.hpp"
+//#include "practica3/Forward.hpp"
 
 #include "ros/ros.h"
 
@@ -7,18 +7,18 @@ int main (int argc, char **argv)
   ros::init(argc, argv, "forward");
   ros::NodeHandle n;
 
-  practica3::Forward forward;
+  //practica3::Forward forward;
 
   ros::Rate loop_rate(20);
 
   int count = 0;
 
-  while(forward.ok())
-  {
-    forward.step();
+  //while(forward.ok())
+  //{
+    //forward.step();
 
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
+    //ros::spinOnce();
+    //loop_rate.sleep();
+  //}
   return 0;
 }
