814822e96770eb7f42f0d0a38af1ce86b6dbec1f
Carlota Vera <c.verap.2019@alumnos.urjc.es>
2021-03-25 20:15:25 +0100

    se arregla solo
    Signed-off-by: Carlota Vera <c.verap.2019@alumnos.urjc.es>

--- practica3/fsm/practica3.h
+++ practica3/fsm/practica3.h
@@ -1,4 +1,40 @@
+/*********************************************************************
+*  Software License Agreement (BSD License)
+*
+*   Copyright (c) 2018, Intelligent Robotics
+*   All rights reserved.
+*
+*   Redistribution and use in source and binary forms, with or without
+*   modification, are permitted provided that the following conditions
+*   are met:
 
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*    * Redistributions in binary form must reproduce the above
+*      copyright notice, this list of conditions and the following
+*      disclaimer in the documentation and/or other materials provided
+*      with the distribution.
+*    * Neither the name of Intelligent Robotics nor the names of its
+*      contributors may be used to endorse or promote products derived
+*      from this software without specific prior written permission.
+
+*   THIS SOFTWARE IS PROVpractica3ED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*   COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*   INCpractica3ENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*   POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Francisco Martín fmrico@gmail.com */
+
+/* Mantainer: Francisco Martín fmrico@gmail.com */
 #ifndef PRACTICA3_H_
 #define PRACTICA3_H_
 
@@ -11,7 +47,7 @@
 
 namespace bica
 {
-class practica3: public bica::Component
+class practica3 : public bica::Component
 {
 public:
   practica3();
@@ -19,19 +55,19 @@ public:
 
   void activateCode();
 
-  	virtual void ToYellGoal_code_iterative() {};
+  	virtual void ToBall_code_iterative() {};
+	virtual void ToBall_code_once() {};
+	virtual void ToYellGoal_code_iterative() {};
 	virtual void ToYellGoal_code_once() {};
-	virtual void ToBlueGoal_code_iterative() {};
-	virtual void ToBlueGoal_code_once() {};
 	virtual void Turn_code_iterative() {};
 	virtual void Turn_code_once() {};
-	virtual void ToBall_code_iterative() {};
-	virtual void ToBall_code_once() {};
+	virtual void ToBlueGoal_code_iterative() {};
+	virtual void ToBlueGoal_code_once() {};
 
-  	virtual bool Turn_2_ToBall() {return false;};
-	virtual bool ToBlueGoal_2_ToYellGoal() {return false;};
+  	virtual bool ToYellGoal_2_Turn() {return false;};
 	virtual bool ToBall_2_ToBlueGoal() {return false;};
-	virtual bool ToYellGoal_2_Turn() {return false;};
+	virtual bool ToBlueGoal_2_ToYellGoal() {return false;};
+	virtual bool Turn_2_ToBall() {return false;};
 
 
   bool ok();
@@ -43,16 +79,16 @@ private:
   void step() {}
 
   	void deactivateAllDeps();
-	void ToYellGoal_activateDeps();
-	void ToBlueGoal_activateDeps();
-	void Turn_activateDeps();
 	void ToBall_activateDeps();
+	void ToYellGoal_activateDeps();
+	void Turn_activateDeps();
+	void ToBlueGoal_activateDeps();
 
 
-  	static const int TOYELLGOAL = 0;
-	static const int TOBLUEGOAL = 1;
+  	static const int TOBALL = 0;
+	static const int TOYELLGOAL = 1;
 	static const int TURN = 2;
-	static const int TOBALL = 3;
+	static const int TOBLUEGOAL = 3;
 
 
   int state_;
@@ -60,7 +96,6 @@ private:
   std::string myBaseId_;
   ros::NodeHandle nh_;
   ros::Publisher state_pub_;
-
 };
 
 } /* namespace bica */
